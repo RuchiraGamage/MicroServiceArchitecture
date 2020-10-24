1d3f2a230e66f3d5be0d59927dc83e60432bf30b
ruchira <htruchira@gmail.com>
2020-10-24 14:12:18 +0530

    add refresh configuration end point to consistency

--- configClient/src/main/java/com/ruchira/configClient/RateController.java
+++ configClient/src/main/java/com/ruchira/configClient/RateController.java
@@ -1,6 +1,7 @@
 package com.ruchira.configClient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Ruchira on 10/22/2020.
  */
 @Controller
+@RefreshScope
 public class RateController {
     @Value("${rate}")
     String rate;
